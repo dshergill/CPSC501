c2b63d1d2715f4c6e02e1c86637a65f067b824cf
dshergill <diljotshergill@hotmail.com>
2016-10-14 00:13:34 -0600

    Added initial test classes

---
+++ Assignment 1/tests/ManagerTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+import code.Manager;
+
+public class ManagerTest
+{
+	@Test
+	public void testMng()
+	{}
+}
