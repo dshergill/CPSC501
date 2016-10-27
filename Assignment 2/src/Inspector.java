ceb25e76dd5d60cc25ec261b69ea39cdc00ad918
dshergill <diljotshergill@hotmail.com>
2016-10-27 17:43:20 -0600

    Implemented interface inspector

--- Assignment 2/src/Inspector.java
+++ Assignment 2/src/Inspector.java
@@ -8,10 +8,14 @@ public class Inspector {
 	public void inspect (Object obj, boolean recursive) {
 		
 		Class classObject = obj.getClass();
+		//Gets the class name
 		String className = classObject.getName();
+		//Gets the superclass name
 		if (classObject.getSuperclass() != null) {
 			String superClassName = classObject.getSuperclass().getName();
 		}
+		//Gets the names of the interfaces
+		Class[] classInterfaces = classObject.getInterfaces();
 		
 		
 	}
