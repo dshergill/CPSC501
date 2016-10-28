2fbc514447ce8a49c101b95f4c3903cbea29182c
dshergill <diljotshergill@hotmail.com>
2016-10-27 18:05:14 -0600

    Extracted multiple methods

--- Assignment 2/src/Inspector.java
+++ Assignment 2/src/Inspector.java
@@ -5,32 +5,48 @@ public class Inspector {
 	
 	public Inspector() {}
 	
-	public void inspect (Object obj, boolean recursive) {
+	public void inspect (Object obj, boolean recursive) 
+	{
 		
 		Class classObject = obj.getClass();
+		if (classObject.getSuperclass() != null) {
+			inspectClassName(obj, classObject, recursive);
+			inspectSuperclassName(obj, classObject, recursive);
+			inspectClassInterfaces(obj, classObject, recursive);
+			inspectClassMethods(obj, classObject, recursive);
+		}
 		
+
+		
+	}
+	
+	private void inspectClassName(Object obj, Class classObject, boolean recursive) 
+	{
 		//Gets the class name
 		String className = classObject.getName();
 		System.out.println(className);
-		//Gets the superclass name
-		if (classObject.getSuperclass() != null) {
-			String superClassName = classObject.getSuperclass().getName();
-			System.out.println(superClassName);
-		}
-		
-		//Gets the names of the interfaces
+	}
+	
+	private void inspectSuperclassName(Object obj, Class classObject, boolean recursive)
+	{
+		String superClassName = classObject.getSuperclass().getName();
+		System.out.println(superClassName);
+	}
+	
+	private void inspectClassInterfaces(Object obj, Class classObject, boolean recursive)
+	{
 		Class[] classInterfaces = classObject.getInterfaces();
 		for (int i=0;i<classInterfaces.length; i++){
 			System.out.println(classInterfaces[i]);
 		}
-		
-		//Sets up an array of methods declared by the class object
-		//Method[] classMethods = classObject.getDeclaredMethods();
-		
-		//for(int i = 0; i<classMethods.length; i++){
-			
-		//}
-		
 	}
 	
+	private void inspectClassMethods(Object obj, Class classObject, boolean recursive)
+	{
+		Method[] classMethods = classObject.getDeclaredMethods();
+		
+		for(int i = 0; i<classMethods.length; i++){
+			
+		}
+	}
 }
