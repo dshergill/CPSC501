c81d920a161e74a8f8074831e3f8829fba35427e
dshergill <diljotshergill@hotmail.com>
2016-10-27 18:14:31 -0600

    Fixed output strings

--- Assignment 2/src/Inspector.java
+++ Assignment 2/src/Inspector.java
@@ -7,37 +7,33 @@ public class Inspector {
 	
 	public void inspect (Object obj, boolean recursive) 
 	{
-		
 		Class classObject = obj.getClass();
+		
 		if (classObject.getSuperclass() != null) {
 			inspectClassName(obj, classObject, recursive);
 			inspectSuperclassName(obj, classObject, recursive);
 			inspectClassInterfaces(obj, classObject, recursive);
 			inspectClassMethods(obj, classObject, recursive);
 		}
-		
-
-		
 	}
 	
 	private void inspectClassName(Object obj, Class classObject, boolean recursive) 
 	{
-		//Gets the class name
 		String className = classObject.getName();
-		System.out.println(className);
+		System.out.println("Class name: " + className);
 	}
 	
 	private void inspectSuperclassName(Object obj, Class classObject, boolean recursive)
 	{
 		String superClassName = classObject.getSuperclass().getName();
-		System.out.println(superClassName);
+		System.out.println("Super class name: " + superClassName);
 	}
 	
 	private void inspectClassInterfaces(Object obj, Class classObject, boolean recursive)
 	{
 		Class[] classInterfaces = classObject.getInterfaces();
 		for (int i=0;i<classInterfaces.length; i++){
-			System.out.println(classInterfaces[i]);
+			System.out.println("Implements Interface: " + classInterfaces[i].getName());
 		}
 	}
 	
