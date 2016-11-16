8fee0886d85e74e5bba49b7a1dcb4190d3cdc4a0
dshergill <diljotshergill@hotmail.com>
2016-11-15 19:57:25 -0700

    Added serializable functionality

--- Assignment 3/src/code/Serializer.java
+++ Assignment 3/src/code/Serializer.java
@@ -1,10 +1,19 @@
 package code;
 
 import java.lang.reflect.*;
+
 import java.util.*;
 import org.jdom2.Document;
 import org.jdom2.Element;
 
 public class Serializer {
+	public static Document serializeObject(Object source) throws Exception {
+		return serializeHelper(source, new Document(new Element("serialized")), new IdentityHashMap());
+	}
 
+	private static Document serializeHelper(Object source, Document document, IdentityHashMap identityHashMap) {
+		String id = Integer.toString(identityHashMap.size());
+		identityHashMap.put(source, id);
+		return null;
+	}
 }
