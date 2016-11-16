58289100cc15e35090961e33916abd3d80990cee
dshergill <diljotshergill@hotmail.com>
2016-11-15 20:28:21 -0700

    Initialied Deserializer

--- Assignment 3/src/code/Deserializer.java
+++ Assignment 3/src/code/Deserializer.java
@@ -6,6 +6,23 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 
 public class Deserializer {
+	public static Object deserialize( Document source ) throws Exception
+	{
+		List objList = source.getRootElement().getChildren();
+		Map table = new HashMap();
+		createInstances( table, objList );
+		assignFieldValues( table, objList );
+		return table.get("0");
+	}
 
+	private static void assignFieldValues(Map table, List objList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void createInstances(Map table, List objList) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
